96ecbf4629c4df09a151a25440d81232239d8116
simo-sari07 <simosai100@gmail.com>
2025-03-04 11:46:30 +0100

    fix

--- src/components/Whatsapp/Whatsapp.jsx
+++ src/components/Whatsapp/Whatsapp.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client"
 
 import { useState } from "react"
@@ -131,31 +130,3 @@ export default function WhatsappContact() {
   )
 }
 
-=======
-import { FaWhatsapp } from "react-icons/fa";
-import { motion } from "framer-motion"; 
-
-export default function Whatsapp() {
-  return (
-    <motion.a
-      target="_blank"
-      href="https://wa.me/+212675775884"
-      className="fixed md:bottom-6 bottom-14 right-2  p-3 rounded-full  z-30"
-      initial={{ y: -100, opacity: 0.2 }}
-      animate={{ y: [0, -15, 0], opacity: 1 }}
-      transition={{
-        duration: 1.2,
-        ease: [0.17, 0.67, 0.83, 0.67],
-        repeat: Infinity,
-        repeatDelay: 2,
-      }}
-      whileHover={{
-        scale: 1.1,
-      }}
-      whileTap={{ scale: 0.9 }}
-    >
-      <FaWhatsapp size={45} className="text-green-500" />
-    </motion.a>
-  );
-}
->>>>>>> 592b49770a975352a2ef3c19617c2c40573f8f3b
