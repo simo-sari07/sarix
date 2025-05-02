44bc4edec00ddb31def3db1af5e96be8f66a1417
simo-sari07 <150151322+simo-sari07@users.noreply.github.com>
2025-05-02 04:41:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Resume from "./components/resumex/Resume";
+import Servies from "./components/Services";
 import Projects from "./components/Works/Projects";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/hero";
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Hero />} />
           <Route path="Resume" element={<Resume />} />
+          <Route path="Servies" element={<Servies />} />
           <Route path="projects" element={<Projects />} />
           <Route path="contact" element={<Contact />} />
         </Route>
